23e3602378abd1b9f7d64f27e02f9b17dc79c783
Eric Rochester <err8n@virginia.edu>
2012-05-25 11:14:40 -0400

    Use digitalWrite.

--- AtomLED.hs
+++ AtomLED.hs
@@ -10,18 +10,32 @@ import Language.Atom
 import Data.Word
 import System.Directory
 
+ledPin :: Word8
+ledPin = 8
+
+high :: Word8
+high = 255
+
+low :: Word8
+low = 0
+
+digitalWrite :: Word8 -> Atom ()
+digitalWrite = action dw . (:[]) . ue . Const
+    where
+        dw [l] = "digitalWrite(" ++ show ledPin ++ "," ++ l ++ ")"
+
 -- | Simple Atom to toggle an LED, leaving it on 8 times as long as it's off.
 blink :: Atom ()
 blink = do
   on <- bool "on" True
   period ph $ phase 0 $ atom "blinkOn" $ do
-    call "avr_blink"
+    digitalWrite 255
     on <== not_ (value on)
   period ph $ phase (quot ph 8) $ atom "blinkOff" $ do
-    call "avr_blink"
+    digitalWrite 0
     on <== not_ (value on)
   where
-    ph = 100000
+    ph = 50000
 
 -- | Invoke the Atom compiler.
 main :: IO ()
@@ -34,7 +48,7 @@ main = do
     varInit t var val = cType t ++ " " ++ var ++ " = " ++ val ++ ";"
     prePostCode _ _ _ =
       ( unlines
-        [ (varInit Int16 "ledPin" "13")
+        [ (varInit Int16 "ledPin" $ show ledPin)
         , "void avr_blink(void);"
         ]
       , unlines
