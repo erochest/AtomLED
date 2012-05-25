de15c9d7e273bbe8b15c272257b1819c7cca5805
Eric Rochester <err8n@virginia.edu>
2012-05-25 11:17:24 -0400

    Reformatting.

--- AtomLED.hs
+++ AtomLED.hs
@@ -27,41 +27,38 @@ digitalWrite = action dw . (:[]) . ue . Const
 -- | Simple Atom to toggle an LED, leaving it on 8 times as long as it's off.
 blink :: Atom ()
 blink = do
-  on <- bool "on" True
-  period ph $ phase 0 $ atom "blinkOn" $ do
-    digitalWrite 255
-    on <== not_ (value on)
-  period ph $ phase (quot ph 8) $ atom "blinkOff" $ do
-    digitalWrite 0
-    on <== not_ (value on)
-  where
-    ph = 50000
+    on <- bool "on" True
+    period ph $ phase 0 $ atom "blinkOn" $ do
+        digitalWrite 255
+        on <== not_ (value on)
+    period ph $ phase (quot ph 8) $ atom "blinkOff" $ do
+        digitalWrite 0
+        on <== not_ (value on)
+    where ph = 50000
 
 -- | Invoke the Atom compiler.
 main :: IO ()
 main = do
-  (sch, _, _, _, _) <- compile atomName defaults {cCode = prePostCode} blink
-  putStrLn $ reportSchedule sch
-  renameFile (atomName ++ ".c") (atomName ++ ".pde")
-  where
-    atomName = "AtomLED"
-    varInit t var val = cType t ++ " " ++ var ++ " = " ++ val ++ ";"
-    prePostCode _ _ _ =
-      ( unlines
-        [ (varInit Int16 "ledPin" $ show ledPin)
-        , "void avr_blink(void);"
-        ]
-      , unlines
-        [ "void setup() {"
-        , "  // initialize the digital pin as an output:"
-        , "  pinMode(ledPin, OUTPUT);"
-        , "}"
-        , ""
-        , "// set the LED on or off"
-        , "void avr_blink() { digitalWrite(ledPin, state.AtomLED.on); }"
-        , ""
-        , "void loop() {"
-        ,  "  " ++ atomName ++ "();"
-        , "}"
-        ]
-      )
+    (sch, _, _, _, _) <- compile atomName defaults {cCode = prePostCode} blink
+    putStrLn $ reportSchedule sch
+    renameFile (atomName ++ ".c") (atomName ++ ".pde")
+    where
+        atomName = "AtomLED"
+        varInit t var val = cType t ++ " " ++ var ++ " = " ++ val ++ ";"
+        prePostCode _ _ _ =
+            ( unlines [ (varInit Int16 "ledPin" $ show ledPin)
+                      , "void avr_blink(void);"
+                      ]
+            , unlines [ "void setup() {"
+                      , "  // initialize the digital pin as an output:"
+                      , "  pinMode(ledPin, OUTPUT);"
+                      , "}"
+                      , ""
+                      , "// set the LED on or off"
+                      , "void avr_blink() { digitalWrite(ledPin, state.AtomLED.on); }"
+                      , ""
+                      , "void loop() {"
+                      ,  "  " ++ atomName ++ "();"
+                      , "}"
+                      ]
+            )
